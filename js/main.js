93e33ada0b44bd95690d68be31e9d77225f935a7
huu <trandaihuu4766@gmail.com>
2025-03-01 22:16:56 +0700

    fix bugs  header

--- js/main.js
+++ js/main.js
@@ -6,3 +6,10 @@ console.log(ListPlus);
 const Click = IconPlus.addEventListener("click", () => {
   ListPlus.classList.toggle("active");
 });
+
+document.addEventListener("click", (e) => {
+  // Kiểm tra xem click có nằm trong #list-plus hoặc #plus-icon không
+  if (!ListPlus.contains(e.target) && e.target !== IconPlus) {
+    ListPlus.classList.remove("active");
+  }
+});
